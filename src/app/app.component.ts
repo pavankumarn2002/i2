460255f1f65f99bc8effd79e4585bb7c44ace9d5
pavankumarn2002 <pavankumar.n2002@gmail.com>
2022-07-06 09:56:46 +0530

    updated with app-comp.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,12 @@ export class AppComponent {
   constructor(private ex: ExternalService) {
     this.ex.load(
       "https://res.cloudinary.com/veraion9/raw/upload/v1657016121/i2/jq%28new%29/pre.js",
-    )
-  }
-  ngAfterViewInit() {
-    this.ex.load(
       "https://res.cloudinary.com/veraion9/raw/upload/v1657016045/i2/jq%28new%29/index.js",
       "https://res.cloudinary.com/veraion9/raw/upload/v1657015996/i2/jq%28new%29/cell.js",
       "https://res.cloudinary.com/veraion9/raw/upload/v1657016090/i2/jq%28new%29/input.js",
       "https://res.cloudinary.com/veraion9/raw/upload/v1657016158/i2/jq%28new%29/slide.js",
       "https://res.cloudinary.com/veraion9/raw/upload/v1657016189/i2/jq%28new%29/tabs.js"
-    );
+    )
   }
+
 }
