16ac0af8aa3629af6935cbd5167f4ec9d8c568b2
pavankumarn2002 <pavankumar.n2002@gmail.com>
2022-07-07 14:00:51 +0530

    Auto-generated commit [ci skip]

--- dist/i2/assets/js/pre.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.pre').each(function () {
-        var $this = $(this);
-        var t = $this.html();
-        $this.text(t.replace('<', '<').replace('>', '>').replace(/>/g, '>\n'));
-    });
-    $('.pre').css('text-align', 'center');
-})
