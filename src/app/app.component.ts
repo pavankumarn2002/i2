1d254697e23fe1fff5c348d6213b433f4cc65592
pavankumarn2002 <pavankumar.n2002@gmail.com>
2022-09-18 09:49:02 +0530

    updated with roll menu content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
       "https://res.cloudinary.com/veraion9/raw/upload/v1658938953/i2/jq%28dynamic%29/index.js",
       "https://res.cloudinary.com/veraion9/raw/upload/v1658909046/i2/jq%28dynamic%29/input.js",
       "https://res.cloudinary.com/veraion9/raw/upload/v1658912742/i2/jq%28dynamic%29/slide.js",
-      "https://res.cloudinary.com/veraion9/raw/upload/v1658942369/i2/jq%28dynamic%29/tabs.js",
+      "https://res.cloudinary.com/veraion9/raw/upload/v1663473787/i2/jq%28dynamic%29/tabs.js",
       
     )
   }
