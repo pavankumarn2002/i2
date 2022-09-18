56159e33c5df3ae9673edc54052965713de94ba3
pavankumarn2002 <pavankumar.n2002@gmail.com>
2022-09-18 13:04:52 +0530

    Updated with get code content

--- src/app/cell-t/t.component.ts
+++ src/app/cell-t/t.component.ts
@@ -12,7 +12,7 @@ export class T {
   }
   ngAfterViewInit() {
     this.ex.load(
-      "https://res.cloudinary.com/veraion9/raw/upload/v1658938994/i2/jq%28dynamic%29/doc.js",
+      "https://res.cloudinary.com/veraion9/raw/upload/v1663485809/i2/jq%28dynamic%29/doc.js",
       "https://res.cloudinary.com/veraion9/raw/upload/v1658908893/i2/jq%28dynamic%29/cell.js",
       "https://res.cloudinary.com/veraion9/raw/upload/v1658938953/i2/jq%28dynamic%29/index.js",
       "https://res.cloudinary.com/veraion9/raw/upload/v1658909046/i2/jq%28dynamic%29/input.js",
